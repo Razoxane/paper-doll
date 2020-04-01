bac7455ec6731c3da3e1ebe375ce1f8e4d6a168b
Jade Edward <jade@jadeedward.com>
2020-04-01 18:35:44 +1100

    Fixing main merge.

--- main.js
+++ main.js
@@ -17,10 +17,15 @@ $(function() {
         html2canvas($("#doll"), {
             onrendered: function(canvas) {
                 theCanvas = canvas;
+<<<<<<< HEAD
 
                 var theDialog = $('<div id="theDialog" title="Save your Combo!"><p>To save, right-click on the image, and choose <strong>"Save Image As..."</strong> from the context menu.</p><p>After that you can share your combo in <a href="https://www.facebook.com/gwyllemclothing/posts/1520700024772010" target="_blank">this thread on the Gwyllem Facebook page</a>!</p></div>')
                 .append(canvas);
 
+=======
+                var theDialog = $('<div id="theDialog" title="Save your Combo!"><p>To save, right-click on the image, and choose <strong>"Save Image As..."</strong> from the context menu.</p><p>After that you can share your combo in <a href="https://www.facebook.com/gwyllemclothing/posts/1520700024772010" target="_blank">this thread on the Gwyllem Facebook page</a>!</p></div>')
+                .append(canvas);
+>>>>>>> 0ca58bf35129171006495dc468b2ca4ee00dcb9d
                 theDialog.dialog({
                     modal: true,
                     width: 550,
