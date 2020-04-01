0df22b1130831289799cd5b46e830d9e79a1e044
Jade Edward <jade@jadeedward.com>
2020-04-01 18:34:51 +1100

    Add the ability to save an image of your combo

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 $(function() {
     $('.top_switch').on('click', function(){
         $('.top_switch').removeAttr('selected');
@@ -14,4 +13,21 @@ $(function() {
         $('.skirt_container').css('background-image','url(' + bg + ')');
     });
 
+    $('.save').on('click', function() {
+        html2canvas($("#doll"), {
+            onrendered: function(canvas) {
+                theCanvas = canvas;
+
+                var theDialog = $('<div id="theDialog" title="Save your Combo!"><p>To save, right-click on the image, and choose <strong>"Save Image As..."</strong> from the context menu.</p><p>After that you can share your combo in <a href="https://www.facebook.com/gwyllemclothing/posts/1520700024772010" target="_blank">this thread on the Gwyllem Facebook page</a>!</p></div>')
+                .append(canvas);
+
+                theDialog.dialog({
+                    modal: true,
+                    width: 550,
+                    height: 800
+                });
+
+            }
+        });
+    })
 });
